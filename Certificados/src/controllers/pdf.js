1e9d7f522d29e0f7597ba1990395da60d7c75deb
yuliano <yulianosalvatore2001@gmail.com>
2020-09-21 13:38:32 -0500

    diseño del pdf

--- Certificados/src/controllers/pdf.js
+++ Certificados/src/controllers/pdf.js
@@ -28,22 +28,106 @@ const getUsersPdf = async (req, res,db) => {
 
             //texto
             pdf.moveDown()
-                .fillColor('black')
-                .font('Times-Roman')
-                .fontSize(30)
-                .text('Nombre: ' + nombre + ' ' + apellido, 85, 220, {
-                    align: 'center',
-                    width: 600,
-                    indent: 30,
-                    height: 200,
-                    ellipsis: true
-                });
-            pdf.moveDown()
-                .fillColor('black')
+            .fillColor('black')
 
+            .font('Times-Roman')
+            .fontSize(20)
+            .text(' ' + id, 87, 160, {
+                align: 'center',
+                width: 600,
+                indent: 30,
+                height: 200,
+                ellipsis: true
+            });
+            pdf.moveDown()
+                .fillColor('black')
                 .font('Times-Roman')
-                .fontSize(30)
-                .text('Cedula: ' + id, {
+                .fontSize(20)
+                .text('' + nombre + ' ' + apellido, {
+                    align: 'center',
+                    width: 600,
+                    indent: 30,
+                    height: 200,
+                    ellipsis: true
+                });
+               
+
+           
+            pdf.moveDown()
+                .fillColor('black')
+                .font('Times-Roman')
+                .fontSize(20)
+                .text(' ' + empresa, {
+                    align: 'center',
+                    width: 600,
+                    indent: 30,
+                    height: 200,
+                    ellipsis: true
+                });
+            pdf.moveDown()
+                .fillColor('black')
+                .font('Times-Roman')
+                .fontSize(20)
+                .text(' ' + email, {
+                    align: 'center',
+                    width: 600,
+                    indent: 30,
+                    height: 200,
+                    ellipsis: true
+                });
+                
+                pdf.moveDown()
+                .fillColor('black')
+                .font('Times-Roman')
+                .fontSize(11)
+                .text('Asistio a ',{
+                    align: 'center',
+                    width: 600,
+                    indent: 30,
+                    height: 200,
+                    ellipsis: true
+                });
+                pdf.moveDown()
+                .fillColor('black')
+                .font('Times-Roman')
+                .fontSize(20)
+                .text('Evento Cucuta 2019',{
+                    align: 'center',
+                    width: 600,
+                    indent: 30,
+                    height: 200,
+                    ellipsis: true
+                });
+                //firma helena 
+                pdf.moveDown()
+                .image('./src/img/firmahelena.png', 305, 400, { width: 200 })
+                pdf.moveDown()
+                .fillColor('black')
+                .font('Times-Roman')
+                .fontSize(11)
+                .text(' ' , {
+                    align: 'center',
+                    width: 600,
+                    indent: 30,
+                    height: 200,
+                    ellipsis: true
+                });
+                pdf.moveDown()
+                .fillColor('black')
+                .font('Times-Roman')
+                .fontSize(11)
+                .text(' Helena Gutiérrez' , {
+                    align: 'center',
+                    width: 600,
+                    indent: 30,
+                    height: 200,
+                    ellipsis: true
+                });
+                pdf.moveDown()
+                .fillColor('black')
+                .font('Times-Roman')
+                .fontSize(11)
+                .text(' Presidente' , {
                     align: 'center',
                     width: 600,
                     indent: 30,
@@ -51,28 +135,6 @@ const getUsersPdf = async (req, res,db) => {
                     ellipsis: true
                 });
 
-            pdf.moveDown()
-                .fillColor('black')
-                .font('Times-Roman')
-                .fontSize(30)
-                .text('Empresa: ' + empresa, {
-                    align: 'center',
-                    width: 600,
-                    indent: 30,
-                    height: 200,
-                    ellipsis: true
-                });
-            pdf.moveDown()
-                .fillColor('black')
-                .font('Times-Roman')
-                .fontSize(30)
-                .text('Email: ' + email, {
-                    align: 'center',
-                    width: 600,
-                    indent: 30,
-                    height: 200,
-                    ellipsis: true
-                });
            // descargar pdf
             pdf.pipe(fs.createWriteStream(__dirname + '../../../Certificado.pdf'));
             console.log('Archivo creado satisfactoriamente ....');
